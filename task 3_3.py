4a11b5de258ecb635afa6c46c1acebfd24683519
kopilka523 <sergeyshcherbina523@gmail.com>
2022-04-05 11:08:39 +0300

    dz_4

--- task 3_3.py
+++
@@ -1,9 +0,0 @@
-def thesaurus(*names):
-    our_dict = {}
-    for n in names:
-        our_dict.setdefault(n[0], [])
-        our_dict[n[0]].append(n)
-        #our_dict_1.setdefault(n[8], [])
-        #our_dict_1[n[8]].append(n)
-    return our_dict
-print(thesaurus("Иван", "Мария", "Петр", "Илья"))
